8609e877e65ed301f473ff8d25f17f1b4cf21c53
Stephan Kleine <stephan.kleine@gmail.com>
2014-06-10 21:23:13 +0200

    add first logstash integration

--- docker_logstash_forwarder.go
+++ docker_logstash_forwarder.go
@@ -1,18 +1,124 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"io/ioutil"
 	"log"
-	"sync"
 	"os"
+	"sync"
 
 	docker "github.com/fsouza/go-dockerclient"
+	"fmt"
 )
 
 var (
+	configFile     string
 	dockerEndPoint string
 	wg             sync.WaitGroup
 )
+
+type Network struct {
+	Servers        []string `json:"servers"`
+	SslCertificate string `json:"ssl certificate"`
+}
+
+type File struct {
+	Paths []string `json:"paths"`
+	Fields map[string]string `json:"fields"`
+}
+
+type LogstashForwarderConfig struct {
+	Network Network `json:"network"`
+	Files   []File `json:"files"`
+}
+
+func readLogstashForwarderConfig(path string) (*LogstashForwarderConfig, error) {
+//	log.Printf("Reading logstash-forwarder config from %s", path)
+
+	configFile, err := os.Open(path)
+	defer configFile.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	logstashConfig := new(LogstashForwarderConfig)
+
+	jsonParser := json.NewDecoder(configFile)
+	if err = jsonParser.Decode(&logstashConfig); err != nil {
+		return nil, err
+	}
+
+	return logstashConfig, nil
+}
+
+func generateConfig(client *docker.Client) error {
+	log.Println("Generating configuration...")
+	globalConfig, err := readLogstashForwarderConfig(configFile)
+	if err != nil {
+		log.Fatalf("Unable to read logstash-forwarder config from %s: %s", configFile, err)
+	}
+	log.Printf("Using logstash-forwarder config from %s as template", configFile)
+
+	containers, err := client.ListContainers(docker.ListContainersOptions{All: false})
+	if err != nil {
+		log.Fatalf("Unable to retrieve container list from docker: %s", err)
+	}
+
+	log.Printf("Found %d containers:", len(containers))
+	for i, container := range containers {
+		log.Printf("%d. %s", i+1, container.ID)
+
+		containerConfig, err := getLogstashForwarderConfigForContainer(container.ID)
+		if err != nil {
+			log.Printf("No logstash-forwarder config found in %s: %s", container.ID, err)
+		} else {
+			log.Printf("Found logstash-forwarder config in %s", container.ID)
+
+			hostname := getHostnameForContainer(container.ID)
+			log.Printf("Hostname: %s", hostname)
+
+			for _, file := range containerConfig.Files {
+				log.Printf("Adding file %s of type %s", file.Paths, file.Fields["type"])
+				file.Fields["host"] = hostname
+				globalConfig.Files = append(globalConfig.Files, file)
+			}
+		}
+
+
+	}
+
+
+	log.Printf("Network: %s", globalConfig.Network.Servers)
+
+	return nil
+}
+
+func getLogstashForwarderConfigForContainer(id string) (*LogstashForwarderConfig, error) {
+	containerDirectory := fmt.Sprintf("/var/lib/docker/btrfs/subvolumes/%s", id)
+	path := fmt.Sprintf("%s/etc/logstash-forwarder.conf", containerDirectory)
+	config, err := readLogstashForwarderConfig(path)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, file := range config.Files {
+		for i, path := range file.Paths {
+			file.Paths[i] = containerDirectory + path
+		}
+	}
+
+	return config, nil
+}
+
+func getHostnameForContainer(id string) string {
+	path := fmt.Sprintf("/var/lib/docker/containers/%s/hostname", id)
+	hostname, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatalf("Unable to read hostname of container %s from %s: %s", id, path, err)
+	}
+	return string(hostname[:])
+}
 
 func listenToDockerEvents(client *docker.Client) {
 	wg.Add(1)
@@ -37,12 +143,14 @@ func listenToDockerEvents(client *docker.Client) {
 
 		if event.Status == "start" || event.Status == "stop" || event.Status == "die" {
 			log.Printf("Received event %s for container %s", event.Status, event.ID[:12])
+			generateConfig(client)
 		}
 	}
 }
 
 func initFlags() {
-	flag.StringVar(&dockerEndPoint, "endPoint", "", "docker api endpoint - defaults to unix:///var/run/docker.sock")
+	flag.StringVar(&dockerEndPoint, "docker", "", "docker api endpoint - defaults to unix:///var/run/docker.sock")
+	flag.StringVar(&configFile, "config", "/etc/logstash-forwarder.conf", "logstash-forwarder config")
 	flag.Parse()
 }
 
@@ -57,16 +165,13 @@ func main() {
 	if err != nil {
 		log.Fatalf("Unable to connect to docker at %s: %s", endpoint, err)
 	}
-	log.Printf("Connected to docker at %s", endpoint)
-
-	containers, _ := client.ListContainers(docker.ListContainersOptions{All: true})
-
-	log.Printf("Found %d containers", len(containers))
-
-	for _, container := range containers {
-		log.Println("ID: ", container.ID)
+	version, err := client.Version()
+	if err != nil {
+		log.Fatalf("Unable to retrieve version information from docker: %s", err)
 	}
+	log.Printf("Connected to docker at %s (v%s)", endpoint, version.Get("Version"))
 
+	generateConfig(client)
 	listenToDockerEvents(client)
 	wg.Wait()
 
