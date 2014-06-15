961a343ec22d0bbaf1864bf71f248682b876d1a2
Stephan Kleine <stephan.kleine@gmail.com>
2014-06-15 02:08:05 +0200

    introduce utils package

--- docker-logstash-forwarder.go
+++ docker-logstash-forwarder.go
@@ -3,11 +3,10 @@ package main
 import (
 	"flag"
 	"log"
-	"os"
 	"sync"
-	"time"
 
 	"github.com/digital-wonderland/docker-logstash-forwarder/forwarder"
+	"github.com/digital-wonderland/docker-logstash-forwarder/utils"
 	docker "github.com/fsouza/go-dockerclient"
 )
 
@@ -17,61 +16,12 @@ var (
 	dockerEndPoint   string
 	laziness         int
 	logstashEndPoint string
-	refresh          configRefresh
 	wg               sync.WaitGroup
 )
 
-type configRefresh struct {
-	mu          sync.Mutex
-	isTriggered bool
-	timer       *time.Timer
-}
-
-func listenToDockerEvents(client *docker.Client) {
-	wg.Add(1)
-	defer wg.Done()
-
-	events := make(chan *docker.APIEvents)
-	defer close(events)
-
-	err := client.AddEventListener((chan<- *docker.APIEvents)(events))
-	if err != nil {
-		log.Fatalf("Unable to add docker event listener: %s", err)
-	}
-	defer client.RemoveEventListener(events)
-
-	log.Println("Listening to docker events...")
-	for {
-		event := <-events
-
-		if event == nil {
-			continue
-		}
-
-		if event.Status == "start" || event.Status == "stop" || event.Status == "die" {
-			log.Printf("Received event %s for container %s", event.Status, event.ID[:12])
-
-			refresh.mu.Lock()
-			if !refresh.isTriggered {
-				log.Printf("Triggering refresh in %d seconds", laziness)
-				refresh.timer = time.AfterFunc(time.Duration(laziness)*time.Second, triggerRefresh)
-				refresh.isTriggered = true
-			}
-			refresh.mu.Unlock()
-		}
-	}
-}
-
-func triggerRefresh() {
-	refresh.mu.Lock()
-	refresh.isTriggered = false
-	refresh.mu.Unlock()
-	forwarder.GenerateConfig(client, getLogstashEndpoint(), configFile)
-}
-
 func initFlags() {
 	flag.StringVar(&dockerEndPoint, "docker", "", "docker api endpoint - defaults to $DOCKER_HOST or unix:///var/run/docker.sock")
-	flag.IntVar(&laziness, "lazyness", 5, "number of seconds to wait after an event for events to accumulate")
+	flag.IntVar(&laziness, "lazyness", 5, "number of seconds to wait after an event before generating new configuration")
 	flag.StringVar(&logstashEndPoint, "logstash", "", "logstash endpoint - defaults to $LOGSTASH_HOST or logstash:5043")
 	flag.StringVar(&configFile, "config", "", "logstash-forwarder config")
 	flag.Parse()
@@ -95,27 +45,25 @@ func main() {
 	}
 	log.Printf("Connected to docker at %s (v%s)", endpoint, version.Get("Version"))
 
-	triggerRefresh()
-	listenToDockerEvents(client)
+	generateConfig()
+	utils.RegisterDockerEventListener(client, generateConfig, &wg, laziness)
 	wg.Wait()
 
 	log.Println("done")
 }
 
+func generateConfig() {
+	log.Printf("Generating new configuration...")
+	utils.Refresh.Mu.Lock()
+	utils.Refresh.IsTriggered = false
+	utils.Refresh.Mu.Unlock()
+	forwarder.GenerateConfig(client, getLogstashEndpoint(), configFile)
+}
+
 func getDockerEndpoint() string {
-	return getEndPoint("unix:///var/run/docker.sock", dockerEndPoint, "DOCKER_HOST")
+	return utils.EndPoint("unix:///var/run/docker.sock", dockerEndPoint, "DOCKER_HOST")
 }
 
 func getLogstashEndpoint() string {
-	return getEndPoint("logstash:5043", logstashEndPoint, "LOGSTASH_HOST")
-}
-
-func getEndPoint(sensibleDefault string, flag string, envVar string) string {
-	if flag != "" {
-		return flag
-	} else if os.Getenv(envVar) != "" {
-		return os.Getenv(envVar)
-	} else {
-		return sensibleDefault
-	}
+	return utils.EndPoint("logstash:5043", logstashEndPoint, "LOGSTASH_HOST")
 }
---
+++ utils/utils.go
@@ -0,0 +1,65 @@
+package utils
+
+import (
+	"log"
+	"os"
+	"sync"
+	"time"
+
+	docker "github.com/fsouza/go-dockerclient"
+)
+
+var (
+	Refresh          ConfigRefresh
+)
+
+type ConfigRefresh struct {
+	Mu          sync.Mutex
+	IsTriggered bool
+	timer       *time.Timer
+}
+
+func EndPoint(sensibleDefault string, flag string, envVar string) string {
+	if flag != "" {
+		return flag
+	} else if os.Getenv(envVar) != "" {
+		return os.Getenv(envVar)
+	} else {
+		return sensibleDefault
+	}
+}
+
+func RegisterDockerEventListener(client *docker.Client, function func(), wg *sync.WaitGroup, laziness int) {
+	wg.Add(1)
+	defer wg.Done()
+
+	events := make(chan *docker.APIEvents)
+	defer close(events)
+
+	err := client.AddEventListener((chan<- *docker.APIEvents)(events))
+	if err != nil {
+		log.Fatalf("Unable to add docker event listener: %s", err)
+	}
+	defer client.RemoveEventListener(events)
+
+	log.Println("Listening to docker events...")
+	for {
+		event := <-events
+
+		if event == nil {
+			continue
+		}
+
+		if event.Status == "start" || event.Status == "stop" || event.Status == "die" {
+			log.Printf("Received event %s for container %s", event.Status, event.ID[:12])
+
+			Refresh.Mu.Lock()
+			if !Refresh.IsTriggered {
+				log.Printf("Triggering refresh in %d seconds", laziness)
+				Refresh.timer = time.AfterFunc(time.Duration(laziness)*time.Second, function)
+				Refresh.IsTriggered = true
+			}
+			Refresh.Mu.Unlock()
+		}
+	}
+}
